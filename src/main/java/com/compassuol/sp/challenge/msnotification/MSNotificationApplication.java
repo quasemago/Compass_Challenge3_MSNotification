2cfa6ea28da47f72ab0c4e4a01ebc12d31cf9e85
quasemago <brunoronningfn@gmail.com>
2024-02-22 17:49:32 -0400

    Projeto java Spring Boot inicial criado

---
+++ src/main/java/com/compassuol/sp/challenge/msnotification/MSNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.msnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSNotificationApplication.class, args);
+    }
+
+}
